addd2aacb64cd8175953a0a555cb7263bd1a9541
Wijnand Modderman-Lenstra <maze@maze.io>
2020-01-27 00:07:11 +0100

    Update Ping
    ESPHome does not respond to ping (bug? expected?), so we request device info in stead.

--- client.go
+++ client.go
@@ -528,6 +528,7 @@ func (c *Client) Ping() error {
 
 // PingTimeout is like ping with a custom timeout.
 func (c *Client) PingTimeout(timeout time.Duration) error {
-	_, err := c.sendAndWaitResponseTimeout(&api.PingRequest{}, api.PingResponseType, timeout)
+	// ESPHome doesn't respond to ping (bug? expected?), so we request device info in stead.
+	_, err := c.sendAndWaitResponseTimeout(&api.DeviceInfoRequest{}, api.DeviceInfoResponseType, timeout)
 	return err
 }
